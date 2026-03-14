3092e8496cb0b123428bda60b4671cc8573c2a11
NguyenBaKy2003 <134308896+NguyenBaKy2003@users.noreply.github.com>
2026-03-15 00:50:13 +0700

    fix

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,14 +1,15 @@
+import { Footer } from "@/presentation/components/layout/Footer"
 import { Header } from "@/presentation/components/layout/Header"
 // import { Footer } from "@/presentation/components/layout/Footer"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col ">
       <Header />
-      <main className="flex-1 bg-gray-50">
+      <main className="flex-1 min-h-screen bg-gray-50">
         {children}
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
